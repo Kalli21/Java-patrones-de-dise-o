37b69b52f45400e9e11e56147cec29699778a8fb
Kalli <kalli_28456@hotmail.com>
2022-05-23 23:09:27 -0500

    patron Singleton y Patter

---
+++ PatronesDiseño/src/SingletonDesign/ScrabbleTestThreads.java
@@ -0,0 +1,18 @@
+package SingletonDesign;
+
+public class ScrabbleTestThreads{	
+	
+	public static void main(String[] args){
+		
+				
+		Runnable getTiles = new GetTheTiles();
+				
+		Runnable getTilesAgain = new GetTheTiles();
+				
+				
+		new Thread(getTiles).start();
+		new Thread(getTilesAgain).start();
+		
+	}
+	
+}
